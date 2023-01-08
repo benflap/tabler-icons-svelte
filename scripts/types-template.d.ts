71da5ab9e4e039bf2927e2da1d509a8e245bd6c3
ben <benflap@protonmail.com>
2023-01-08 17:44:11 -0500

    fix: missing type declaration for `class` prop
    I've also optimized `createTypesFile()` and type-template.d.ts to reduce
    the size of the generated file.

--- scripts/types-template.d.ts
+++ scripts/types-template.d.ts
@@ -1,1 +1,10 @@
 import { SvelteComponentTyped } from "svelte";
+
+type TablerIconProps = {
+    color?: string;
+    size?: string | number;
+    strokeWidth?: string | number;
+    class?: string;
+};
+
+class TablerIcon extends SvelteComponentTyped<TablerIconProps> {}
